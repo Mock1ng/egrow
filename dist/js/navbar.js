c4748a09e0e2f419695b5d2493fd653c31a1d439
Mock1ng <bayuw1123@gmail.com>
2020-06-17 18:20:25 +0700

    add jumbo desk-design

--- dist/js/navbar.js
+++ dist/js/navbar.js
@@ -1,6 +1,11 @@
-const burgerButton = document.querySelector('.hamburger');
-const menuWrapper = document.querySelector('.menu-wrapper');
-
-burgerButton.onclick = () => {
-    menuWrapper.classList.toggle('nav-active')
+const navbarMobile = () => {
+    const burgerButton = document.querySelector('.hamburger');
+    const menuWrapper = document.querySelector('.menu-wrapper');
+    const outLayer = document.querySelector('.outlayer');
+    burgerButton.onclick = () => {
+        menuWrapper.classList.toggle('nav-active');
+        outLayer.classList.toggle('display-block');
+    }
 }
+
+navbarMobile();
