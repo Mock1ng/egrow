32e0b642628421e18ae9ce30955d7bf2fde45fc5
Mock1ng <bayuw1123@gmail.com>
2020-07-09 11:32:59 +0700

    add table @pengajar, jst ignore others

---
+++ dist/js/pengajar.js
@@ -0,0 +1,246 @@
+const data = [{
+        nama: 'Ayu Tira Safitri',
+        jurusan: 'Kimia',
+        alamat: 'Ciputat, Tangerang Selatan',
+        pengalaman: 'Pengajar tematik SD, IPA SMP dan Bahasa Arab dari tahun 2018'
+    },
+    {
+        nama: 'Baywcksn',
+        jurusan: 'Sistem Informasi',
+        alamat: 'Cikupa, Tangerang',
+        pengalaman: 'Pengajar pemrograman walau dalam mimpi'
+    },
+    {
+        nama: 'Akubaru risuki',
+        jurusan: 'Cinta Menggelora',
+        alamat: 'Gatau, Daerah JakSel ae',
+        pengalaman: 'Pengajar bucin-bucin agar segera menjadi fakboi'
+    },
+    {
+        nama: 'Rijuki Rama',
+        jurusan: 'Segala Bidang',
+        alamat: 'Hmm, Depan TMII pokoknya',
+        pengalaman: 'Pengejar kebahagiaan lewat kekesalan'
+    },
+    {
+        nama: 'Hanifu',
+        jurusan: 'Perkosan',
+        alamat: 'Ciputat, TangSel (yoman bener)',
+        pengalaman: 'Pengajar berbudidaya ketika bertamu di kosan orang'
+    },
+    {
+        nama: 'Ayu Tira Safitri',
+        jurusan: 'Kimia',
+        alamat: 'Ciputat, Tangerang Selatan',
+        pengalaman: 'Pengajar tematik SD, IPA SMP dan Bahasa Arab dari tahun 2018'
+    },
+    {
+        nama: 'Baywcksn',
+        jurusan: 'Sistem Informasi',
+        alamat: 'Cikupa, Tangerang',
+        pengalaman: 'Pengajar pemrograman walau dalam mimpi'
+    },
+    {
+        nama: 'Akubaru risuki',
+        jurusan: 'Cinta Menggelora',
+        alamat: 'Gatau, Daerah JakSel ae',
+        pengalaman: 'Pengajar bucin-bucin agar segera menjadi fakboi'
+    },
+    {
+        nama: 'Rijuki Rama',
+        jurusan: 'Segala Bidang',
+        alamat: 'Hmm, Depan TMII pokoknya',
+        pengalaman: 'Pengejar kebahagiaan lewat kekesalan'
+    },
+    {
+        nama: 'Hanifu',
+        jurusan: 'Perkosan',
+        alamat: 'Ciputat, TangSel (yoman bener)',
+        pengalaman: 'Pengajar berbudidaya ketika bertamu di kosan orang'
+    },
+    {
+        nama: 'Ayu Tira Safitri',
+        jurusan: 'Kimia',
+        alamat: 'Ciputat, Tangerang Selatan',
+        pengalaman: 'Pengajar tematik SD, IPA SMP dan Bahasa Arab dari tahun 2018'
+    },
+    {
+        nama: 'Baywcksn',
+        jurusan: 'Sistem Informasi',
+        alamat: 'Cikupa, Tangerang',
+        pengalaman: 'Pengajar pemrograman walau dalam mimpi'
+    },
+    {
+        nama: 'Akubaru risuki',
+        jurusan: 'Cinta Menggelora',
+        alamat: 'Gatau, Daerah JakSel ae',
+        pengalaman: 'Pengajar bucin-bucin agar segera menjadi fakboi'
+    },
+    {
+        nama: 'Rijuki Rama',
+        jurusan: 'Segala Bidang',
+        alamat: 'Hmm, Depan TMII pokoknya',
+        pengalaman: 'Pengejar kebahagiaan lewat kekesalan'
+    },
+    {
+        nama: 'Hanifu',
+        jurusan: 'Perkosan',
+        alamat: 'Ciputat, TangSel (yoman bener)',
+        pengalaman: 'Pengajar berbudidaya ketika bertamu di kosan orang'
+    },
+    {
+        nama: 'Ayu Tira Safitri',
+        jurusan: 'Kimia',
+        alamat: 'Ciputat, Tangerang Selatan',
+        pengalaman: 'Pengajar tematik SD, IPA SMP dan Bahasa Arab dari tahun 2018'
+    },
+    {
+        nama: 'Baywcksn',
+        jurusan: 'Sistem Informasi',
+        alamat: 'Cikupa, Tangerang',
+        pengalaman: 'Pengajar pemrograman walau dalam mimpi'
+    },
+    {
+        nama: 'Akubaru risuki',
+        jurusan: 'Cinta Menggelora',
+        alamat: 'Gatau, Daerah JakSel ae',
+        pengalaman: 'Pengajar bucin-bucin agar segera menjadi fakboi'
+    },
+    {
+        nama: 'Rijuki Rama',
+        jurusan: 'Segala Bidang',
+        alamat: 'Hmm, Depan TMII pokoknya',
+        pengalaman: 'Pengejar kebahagiaan lewat kekesalan'
+    },
+    {
+        nama: 'Hanifu',
+        jurusan: 'Perkosan',
+        alamat: 'Ciputat, TangSel (yoman bener)',
+        pengalaman: 'Pengajar berbudidaya ketika bertamu di kosan orang'
+    },
+    {
+        nama: 'Ayu Tira Safitri',
+        jurusan: 'Kimia',
+        alamat: 'Ciputat, Tangerang Selatan',
+        pengalaman: 'Pengajar tematik SD, IPA SMP dan Bahasa Arab dari tahun 2018'
+    },
+    {
+        nama: 'Baywcksn',
+        jurusan: 'Sistem Informasi',
+        alamat: 'Cikupa, Tangerang',
+        pengalaman: 'Pengajar pemrograman walau dalam mimpi'
+    },
+    {
+        nama: 'Akubaru risuki',
+        jurusan: 'Cinta Menggelora',
+        alamat: 'Gatau, Daerah JakSel ae',
+        pengalaman: 'Pengajar bucin-bucin agar segera menjadi fakboi'
+    },
+    {
+        nama: 'Rijuki Rama',
+        jurusan: 'Segala Bidang',
+        alamat: 'Hmm, Depan TMII pokoknya',
+        pengalaman: 'Pengejar kebahagiaan lewat kekesalan'
+    },
+    {
+        nama: 'Hanifu',
+        jurusan: 'Perkosan',
+        alamat: 'Ciputat, TangSel (yoman bener)',
+        pengalaman: 'Pengajar berbudidaya ketika bertamu di kosan orang'
+    },
+    {
+        nama: 'Ayu Tira Safitri',
+        jurusan: 'Kimia',
+        alamat: 'Ciputat, Tangerang Selatan',
+        pengalaman: 'Pengajar tematik SD, IPA SMP dan Bahasa Arab dari tahun 2018'
+    },
+    {
+        nama: 'Baywcksn',
+        jurusan: 'Sistem Informasi',
+        alamat: 'Cikupa, Tangerang',
+        pengalaman: 'Pengajar pemrograman walau dalam mimpi'
+    },
+    {
+        nama: 'Akubaru risuki',
+        jurusan: 'Cinta Menggelora',
+        alamat: 'Gatau, Daerah JakSel ae',
+        pengalaman: 'Pengajar bucin-bucin agar segera menjadi fakboi'
+    },
+    {
+        nama: 'Rijuki Rama',
+        jurusan: 'Segala Bidang',
+        alamat: 'Hmm, Depan TMII pokoknya',
+        pengalaman: 'Pengejar kebahagiaan lewat kekesalan'
+    },
+    {
+        nama: 'Hanifu',
+        jurusan: 'Perkosan',
+        alamat: 'Ciputat, TangSel (yoman bener)',
+        pengalaman: 'Pengajar berbudidaya ketika bertamu di kosan orang'
+    }
+];
+// Variables
+const table = document.querySelector('table');
+const btnContainer = document.querySelector('.pagination-btn');
+const tbody = document.querySelector('tbody');
+
+// Additions
+let btnCounter = parseInt(Math.ceil(data.length / 10));
+let start = 1;
+let end = 10;
+
+// Functions
+const injectTable = (start, end, data) => {
+    // const tbody = document.createElement('tbody');
+    tbody.innerHTML = '';
+
+    for (let i = start; i <= end; i++) {
+
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <td class="data">${i}</td>
+        <td class="data">${data[i - 1].nama}</td>
+        <td class="data">${data[i - 1].jurusan}</td>
+        <td class="data">${data[i - 1].alamat}</td>
+        <td class="data">${data[i - 1].pengalaman}</td>
+        `;
+
+        tbody.appendChild(tr)
+    }
+
+    table.appendChild(tbody);
+}
+
+const paginationButton = () => {
+    for (let i = 0; i < btnCounter; i++) {
+        const button = document.createElement('button');
+        button.innerText = i + 1;
+        btnContainer.appendChild(button);
+    }
+}
+
+const paging = () => {
+    const buttonSelect = document.querySelectorAll('button');
+    buttonSelect[0].classList.add('active');
+
+    btnContainer.onclick = e => {
+        if (e.target.classList[0] !== 'pagination-btn') {
+            for (let i = 0; i < buttonSelect.length; i++) {
+                buttonSelect[i].classList.remove('active');
+            }
+
+            let whereBtnClicked = e.target.innerText;
+            injectTable(start + (10 * (whereBtnClicked - 1)), end + (10 * (whereBtnClicked - 1)), data);
+
+            buttonSelect[whereBtnClicked - 1].classList.add('active');
+        }
+    }
+}
+
+const wrapper = () => {
+    injectTable(start, end, data);
+    paginationButton();
+    paging();
+}
+
+wrapper();
